2a5e398c228353995fac8b3f483f10078881ca63
Isabella <iwestlauer@gmail.com>
2026-01-25 16:36:13 -0600

    updated NASA API Rmd, and XML Scraping Rmd

--- Unit 4 Tidyverse/api_practice.R
+++ Unit 4 Tidyverse/api_practice.R
@@ -1,15 +1,10 @@
 #api practice
 
 library(httr2)
-# library(tibble)
-# library(dplyr)
-# library(ggplot2)
 library(tidyverse) #includes tibble, dplyr, ggplot2, tidyr, readr, stringr, ...
 
 # Make an API GET request to sbdb_query.api
-
-# fields <- "rms,last_obs"
-fields <- "spkid,rms"
+fields <- "spkid,rms,last_obs"
 limit <- 100 # limit the number of records returned
 
 resp <- request("https://ssd-api.jpl.nasa.gov/sbdb_query.api") |>
@@ -20,21 +15,41 @@ resp$status_code # 200 is good, 400 is a bad request
 
 # parse json response into data object
 data <- resp_body_json(resp)
-str(data, max.level = 2)
+str(data$fields, max.level = 2)
 
 # Convert to data frame, actually a tibble
-
-rms_df <- tibble(
-  rms = as.numeric(unlist(data$data))
+rms_df <- as_tibble(
+  do.call(rbind, data$data),
+  .name_repair = "minimal"
 )
-head(rms_df)
-summarize(rms_df)
-data$fields
+
 # Assign column names
 colnames(rms_df) <- data$fields
 
-# Convert RMS to numeric
-rms_df$rms <- as.numeric(rms_df$rms)
+rms_df <- rms_df |>
+  mutate(
+    spkid = unlist(spkid),
+    rms = unlist(rms),
+    last_obs = unlist(last_obs)
+  )
+
+# peek at last_obs
+unique(rms_df$last_obs)[1:10]
+
+# change the data types, replace N/A with blank
+rms_df <- rms_df |>
+  mutate(
+    spkid = as.character(spkid),
+    rms = as.numeric(rms),
+    last_obs = na_if(last_obs, ""),
+    last_obs = as.Date(last_obs, format = "%Y-%m-%d")
+  )
+
+# inspect
+head(rms_df)
+
+# export to csv
+write.csv(rms_df, file = "output.csv", row.names = FALSE)
 
 # Check
 # head(rms_df)
@@ -42,8 +57,8 @@ str(rms_df)
 
 ggplot(rms_df, aes(x = rms)) +
   geom_histogram(binwidth = 0.05, fill = "blue", color = "black") +
-  labs(title = "Distribution of Orbit RMS values of small bodies", 
-       x = "RMS", 
+  labs(title = "Distribution of Orbit RMS values of Small Bodies",
+       x = "RMS",
        y = "Count") +
   theme_minimal()
 
